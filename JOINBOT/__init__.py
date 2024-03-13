d870f31961645edb07ec447577db29aa2ce3ccd9
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-03-13 09:54:50 +0530

    Update __init__.py

--- JOINBOT/__init__.py
+++ JOINBOT/__init__.py
@@ -5,7 +5,6 @@ API_KEY = 20058505
 API_HASH = "c6416428be72db3174999c1740524b88"
 BOT_TOKEN = "6563744619:AAG4v_ABfLA3lCSGbcNLWlS07ZA_qUmseqM"
 
-loop = asyncio.get_event_loop()
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[logging.FileHandler('logs.txt'), logging.StreamHandler()],
